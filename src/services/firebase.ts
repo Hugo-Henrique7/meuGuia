183573201dbac5022d63843f247d834a7782f558
Seu Nome <seuemail@example.com>
2026-02-04 05:29:13 -0300

    attt projeto

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,9 +1,10 @@
 import { initializeApp, getApp, getApps } from "firebase/app";
-import { initializeAuth } from "firebase/auth";
-import { getReactNativePersistence } from "firebase/auth/react-native";
-import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+
+import { initializeAuth, getReactNativePersistence } from "firebase/auth";
+
+import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
 
 const firebaseConfig = {
   apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY!,
@@ -14,11 +15,15 @@ const firebaseConfig = {
   appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID!,
 };
 
-const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+// Evita inicializar duas vezes no Expo (Hot Reload)
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
 
+// Auth com persistência no React Native (AsyncStorage)
 export const auth = initializeAuth(app, {
   persistence: getReactNativePersistence(ReactNativeAsyncStorage),
 });
 
 export const db = getFirestore(app);
 export const storage = getStorage(app);
+
+export default app;
